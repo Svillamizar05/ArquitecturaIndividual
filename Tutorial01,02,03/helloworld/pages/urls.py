c9315b3b63d862fba84727faac856517fd60c1a0
Santiago <savillamizar05@gmail.com>
2025-08-12 20:14:34 -0500

    Tutorial 3

--- Tutorial01,02,03/helloworld/pages/urls.py
+++ Tutorial01,02,03/helloworld/pages/urls.py
@@ -5,7 +5,10 @@ from .views import (
     ContactPageView,
     ProductIndexView, 
     ProductShowView, 
-    ProductCreateView
+    ProductCreateView,
+    CartView,
+    CartRemoveAllView,
+
 )
 
 urlpatterns = [
@@ -15,6 +18,10 @@ urlpatterns = [
     path("products/", ProductIndexView.as_view(), name="index"),
     path('products/create/', ProductCreateView.as_view(), name='form'),
     path("products/<str:id>", ProductShowView.as_view(), name="show"),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+
 ]
 
 
